033b4cbc05d4744daba89495587e800f055c84ec
abdallah adel <abdallah.aadel123@gmail.com>
2024-01-21 19:20:42 +0200

    add firebase to my app and add cloud fireStore

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/Home/HomeScreen.dart';
 import 'package:todo_app/MyTheme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main(){
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 class MyApp extends StatelessWidget{
